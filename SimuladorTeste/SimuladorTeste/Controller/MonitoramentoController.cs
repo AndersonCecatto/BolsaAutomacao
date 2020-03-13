83d6d6019fc1bc5d360425e87b12f4c8cf76ac7e
Anderson Cecatto de Oliveira <andersoncecatto2009@hotmail.com>
2020-03-12 22:15:15 -0300

    Add files via upload

---
+++ SimuladorTeste/SimuladorTeste/Controller/MonitoramentoController.cs
@@ -0,0 +1,28 @@
+﻿using SimuladorTeste.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimuladorTeste.Controller
+{
+    public class MonitoramentoController
+    {
+        private readonly Model1 _context;
+
+        public MonitoramentoController(Model1 context)
+        {
+            _context = context;
+        }
+
+        public void InserirtMonitoramento(Monitoramento monitoramento)
+        {
+            if (monitoramento != null)
+            {
+                _context.Monitoramentos.Add(monitoramento);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ SimuladorTeste/SimuladorTeste/Models/Monitoramento.cs
@@ -0,0 +1,20 @@
+namespace SimuladorTeste.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Monitoramentos")]
+    public partial class Monitoramento
+    {
+        public int Id { get; set; }
+
+        public string Empresa { get; set; }
+
+        public decimal PrecoCompra { get; set; }
+
+        public decimal PrecoVenda { get; set; }
+    }
+}
